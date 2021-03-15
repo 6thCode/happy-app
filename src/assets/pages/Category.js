7b34799ebc3c5a7a41a10c2619ddc0d3bf87eea0
Anthony Nwaizuzu <tonychuks7@gmail.com>
2021-03-15 17:13:05 +0100

    fetch joke content

--- src/assets/pages/Category.js
+++ src/assets/pages/Category.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
-import JokeContent from "../components/jokeContent";
 import Button from "../components/Button";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "react-loader-spinner";
 import ErrorBoundary from "../components/errorBoundary";
+import { Link } from "react-router-dom";
+import backArrow from '../imgs/arrow-back.png';
 
 function Category(props) {
-  const [isLoadinng, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isLoadinng, setLoading] = useState(false);
   const [CategoryData, setCategoryData] = useState({});
 
   useEffect(() => {
@@ -13,59 +18,74 @@ function Category(props) {
   }, []);
 
   const firstLoad = (category) => {
-    setLoading(false);
-
-    fetch(
+    setLoading(true);
+    const contentUrl =
       "https://v2.jokeapi.dev/joke/" +
-        category +
-        "?blacklistFlags=nsfw,religious,racist,sexist"
-    )
+      category +
+      "?blacklistFlags=nsfw,religious,racist,sexist";
+
+    fetch(`${contentUrl}`)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setCategoryData(data);
-        setLoading(true);
-      });
+      .then(
+        (data) => {
+          setCategoryData(data);
+          setLoading(false);
+        },
+        (error) => {
+          setLoading(true);
+          setError(error);
+        }
+      );
   };
 
-  return (
-    <div>
-      {/* {JSON.stringify(CategoryData)}  */}
-      <ErrorBoundary>
-        <p>{CategoryData.type}</p>
-        <p>
-            { CategoryData.type === "single"
-            ? CategoryData.joke
-            : CategoryData.setup }
-        </p>
-        <p>{CategoryData.type === "twopart" && CategoryData.delivery}</p>
-
-        {/* {CategoryData.type === 'single' &&(
-            <JokeContent
-                type={CategoryData.type}
-                joke={CategoryData.joke}
+  if (error) {
+    toast.error("Failed to load Content");
+  } else {
+    return (
+      <div className="section">
+        <Link className="back-button" to="">
+        <img src={backArrow} width="25" height="25" alt="arrow" />
+        </Link>
+        <ErrorBoundary>
+          {isLoadinng ? (
+            <Loader
+              className="center-loader"
+              type="ThreeDots"
+              color="#595ecd"
+              height={50}
+              width={50}
+              timeout={3000}
             />
-        )}
+          ) : (
+            <div className="joke-section">
+              {CategoryData.setup ? (
+                <>
+                  <p>{CategoryData.type}</p>
+                  <p>{CategoryData.setup}</p>
+                  <p>{CategoryData.delivery}</p>
+                </>
+              ) : (
+                <>
+                  <p>{CategoryData.type}</p>
+                  <p>{CategoryData.joke}</p>
+                </>
+              )}
 
-        {CategoryData.type === 'twopart' &&(
-            <JokeContent
-                type={CategoryData.type}
-                setup={CategoryData.setup}
-                delivery={CategoryData.delivery}
-            />
-        )} */}
-
-        <div className="w-25">
-          <Button
-            title="Randomize"
-            handleClick={() => {
-              firstLoad(props.computedMatch.params.categoryId);
-            }}
-          />
-        </div>
-      </ErrorBoundary>
-    </div>
-  );
+              <div className="">
+                <Button
+                  title="Randomize"
+                  handleClick={() => {
+                    firstLoad(props.computedMatch.params.categoryId);
+                  }}
+                />
+              </div>
+            </div>
+          )}
+          <ToastContainer />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default Category;
--- src/assets/pages/Main.js
+++ src/assets/pages/Main.js
@@ -2,43 +2,66 @@ import React, { useState, useEffect } from "react";
 import ErrorBoundary from "../components/errorBoundary";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import Loader from "react-loader-spinner";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function Main(props) {
-  const [isLoadinng, setLoading] = useState(true);
+function Main() {
+  const [error, setError] = useState(null);
+  const [isLoadinng, setLoading] = useState(false);
   const [CategoryData, setCategoryData] = useState({ categories: [] });
+
+  const apiUrl = "https://v2.jokeapi.dev/categories"
 
   useEffect(() => {
     firstLoad();
-  });
+  }, []);
 
   const firstLoad = () => {
-    setLoading(false);
-
-    fetch("https://v2.jokeapi.dev/categories")
+    setLoading(true);
+    fetch(`${apiUrl}`)
       .then((response) => response.json())
-      .then((data) => {
-        setCategoryData({ ...data });
-        setLoading(true);
-      });
+      .then(
+        (data) => {
+          setLoading(false);
+          setCategoryData({ ...data });
+        },
+        (error) => {
+          setLoading(true);
+          setError(error);
+        }
+      );
   };
 
-  return (
-    <div className="container main-sectiion">
-      <ErrorBoundary>
-        {isLoadinng
-          ? "loading..."
-          : Object.keys(CategoryData).length > 0
-          ? CategoryData.categories.map((item, i) => {
+  if (error) {
+    toast.error("Failed to load Content");
+  } else {
+    return (
+      <div className="container main-sectiion">
+        <ErrorBoundary>
+        <h2 className="text-center pb-5">Happy App</h2>
+          {isLoadinng ? (
+            <Loader
+              type="ThreeDots"
+              color="#595ecd"
+              height={50}
+              width={50}
+              timeout={3000}
+            />
+          ) : (
+            CategoryData.categories.map((item, i) => {
               return (
-                <Link key={i} className="w-25" to={"/category/" + item}>
+                <Link key={i} to={"/category/" + item}>
                   <Button title={item} />
                 </Link>
               );
             })
-          : null}
-      </ErrorBoundary>
-    </div>
-  );
+          )}
+          <ToastContainer />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default Main;
